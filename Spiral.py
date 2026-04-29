65220723974b8dd38c8108c5b26c46bfa0b63367
mervan2013 <mervansivarooban2013@gmail.com>
2026-04-28 18:01:24 -0700

    g

---
+++ Spiral.py
@@ -0,0 +1,10 @@
+import turtle
+
+my_wn = turtle.Screen()
+my_wn.bgcolor("Light Blue")
+my_wn.title("Turtle")
+my_pen = turtle.Turtle()
+size + 0
+while True:
+    for (i) in range(4):
+        
---
+++ Square.py
@@ -0,0 +1,7 @@
+import turtle
+
+t = turtle.Turtle()
+for _ in range(4):
+    t.forward(100) # Move forward by 100 units
+    t.left(90)    # Turn 90 degrees left
+turtle.done()
